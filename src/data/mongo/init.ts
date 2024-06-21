324f540963435189b023ea4c3c28528af3210eb7
DarioDiaz <haiverdiaztoro@gmail.com>
2024-06-21 13:11:17 -0500

    prueba a la conexion de mongoDB ok

--- src/data/mongo/init.ts
+++ src/data/mongo/init.ts
@@ -8,18 +8,17 @@ interface ConnectionOptions {
 
 export class MongoDatabase {
 
-  static async connect( options: ConnectionOptions ) {
+  static async connect(options: ConnectionOptions) {
     const { mongoUrl, dbName } = options;
 
     try {
-      
-      await mongoose.connect( mongoUrl, {
+
+      await mongoose.connect(mongoUrl, {
         dbName: dbName,
       });
 
       console.log('Mongo connected!');
-
-
+      return true;
     } catch (error) {
       console.log('Mongo connection error');
       throw error;
